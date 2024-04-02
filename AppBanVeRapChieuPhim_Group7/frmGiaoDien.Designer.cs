54845311d11790cc6cc789d385877827dd3fd85d
DungTran9999 <dungtranvip123@uef.edu.vn>
2024-04-02 23:21:32 +0700

    automatic

--- AppBanVeRapChieuPhim_Group7/frmGiaoDien.Designer.cs
+++ AppBanVeRapChieuPhim_Group7/frmGiaoDien.Designer.cs
@@ -35,6 +35,7 @@
             this.btnSell = new System.Windows.Forms.Button();
             this.plLoadForm = new System.Windows.Forms.Panel();
             this.lbChangeName = new System.Windows.Forms.Label();
+            this.lb8 = new System.Windows.Forms.Label();
             this.plControl.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,19 +49,17 @@
             this.plControl.Dock = System.Windows.Forms.DockStyle.Left;
             this.plControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.plControl.Location = new System.Drawing.Point(0, 0);
-            this.plControl.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.plControl.Name = "plControl";
-            this.plControl.Size = new System.Drawing.Size(173, 706);
+            this.plControl.Size = new System.Drawing.Size(260, 1086);
             this.plControl.TabIndex = 0;
             // 
             // btnExit
             // 
             this.btnExit.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold);
             this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.btnExit.Location = new System.Drawing.Point(2, 622);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExit.Location = new System.Drawing.Point(3, 957);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(169, 82);
+            this.btnExit.Size = new System.Drawing.Size(254, 126);
             this.btnExit.TabIndex = 1;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -69,10 +68,9 @@
             // plScroll
             // 
             this.plScroll.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.plScroll.Location = new System.Drawing.Point(2, 170);
-            this.plScroll.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.plScroll.Location = new System.Drawing.Point(3, 262);
             this.plScroll.Name = "plScroll";
-            this.plScroll.Size = new System.Drawing.Size(8, 103);
+            this.plScroll.Size = new System.Drawing.Size(12, 158);
             this.plScroll.TabIndex = 0;
             // 
             // btnManager
@@ -82,10 +80,9 @@
             this.btnManager.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnManager.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnManager.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.btnManager.Location = new System.Drawing.Point(2, 223);
-            this.btnManager.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnManager.Location = new System.Drawing.Point(3, 343);
             this.btnManager.Name = "btnManager";
-            this.btnManager.Size = new System.Drawing.Size(169, 72);
+            this.btnManager.Size = new System.Drawing.Size(254, 111);
             this.btnManager.TabIndex = 0;
             this.btnManager.Text = "Manager";
             this.btnManager.UseVisualStyleBackColor = false;
@@ -99,10 +96,9 @@
             this.btnSell.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSell.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSell.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.btnSell.Location = new System.Drawing.Point(2, 146);
-            this.btnSell.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSell.Location = new System.Drawing.Point(3, 225);
             this.btnSell.Name = "btnSell";
-            this.btnSell.Size = new System.Drawing.Size(169, 72);
+            this.btnSell.Size = new System.Drawing.Size(254, 111);
             this.btnSell.TabIndex = 0;
             this.btnSell.Text = "Sell";
             this.btnSell.UseVisualStyleBackColor = false;
@@ -112,33 +108,42 @@
             // plLoadForm
             // 
             this.plLoadForm.BackColor = System.Drawing.Color.Gray;
-            this.plLoadForm.Location = new System.Drawing.Point(177, 73);
-            this.plLoadForm.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.plLoadForm.Location = new System.Drawing.Point(266, 112);
             this.plLoadForm.Name = "plLoadForm";
-            this.plLoadForm.Size = new System.Drawing.Size(988, 630);
+            this.plLoadForm.Size = new System.Drawing.Size(1482, 969);
             this.plLoadForm.TabIndex = 1;
             // 
             // lbChangeName
             // 
             this.lbChangeName.AutoSize = true;
             this.lbChangeName.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold);
-            this.lbChangeName.Location = new System.Drawing.Point(221, 26);
-            this.lbChangeName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbChangeName.Location = new System.Drawing.Point(327, 25);
             this.lbChangeName.Name = "lbChangeName";
-            this.lbChangeName.Size = new System.Drawing.Size(108, 41);
+            this.lbChangeName.Size = new System.Drawing.Size(165, 61);
             this.lbChangeName.TabIndex = 2;
             this.lbChangeName.Text = "label1";
             // 
+            // lb8
+            // 
+            this.lb8.AutoSize = true;
+            this.lb8.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb8.ForeColor = System.Drawing.Color.Red;
+            this.lb8.Location = new System.Drawing.Point(657, 44);
+            this.lb8.Name = "lb8";
+            this.lb8.Size = new System.Drawing.Size(386, 35);
+            this.lb8.TabIndex = 3;
+            this.lb8.Text = "Phim Chú Bé Tài 22h tối nay";
+            // 
             // frmGiaoDien
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1169, 706);
+            this.ClientSize = new System.Drawing.Size(1754, 1086);
+            this.Controls.Add(this.lb8);
             this.Controls.Add(this.lbChangeName);
             this.Controls.Add(this.plLoadForm);
             this.Controls.Add(this.plControl);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmGiaoDien";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmGiaoDien";
@@ -159,5 +164,6 @@
         private System.Windows.Forms.Button btnManager;
         private System.Windows.Forms.Panel plScroll;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lb8;
     }
 }
